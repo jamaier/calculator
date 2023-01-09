3d534f088c601c926197d0b43ea1da32d999e8b4
Jacob Maier <jacobamaier@gmail.com>
2023-01-09 13:25:33 -0800

    Add saySomething and add functions to script.js.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+// const result = add(3, 5);
+// window.alert(result);
+
+// const outputText = "The sum is " + result + ".";
+// saySomething(outputText);
+
+saySomething("The sum is " + add(3,5) + ".");
