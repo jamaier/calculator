f5900f9b3b9eed96bfcae7128cf501e5b30ac2cd
Jacob Maier <jacobamaier@gmail.com>
2023-01-09 14:23:41 -0800

    Add funtions for different operators and corosponding window alerts.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 // business logic
 function add(number1, number2) {
   return number1 + number2;
+
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function div(number1, number2) {
+  return number1 / number2;
+}
+
+function mult(number1, number2) {
+  return number1 * number2;
 }
 
 // user interface logic
 const number1 = parseInt(prompt('Enter a number:'));
 const number2 = parseInt(prompt('Enter another number:'));
-window.alert(add(number1, number2));
+
+window.alert("The difference of your numbers =" + subtract(number1, number2)+ ".");
+window.alert("The sum of your numbers =" + add(number1, number2)+ ".");
+window.alert("The remainder of your numbers =" + div(number1, number2)+ ".");
+window.alert("The product of your numbers =" + mult(number1, number2)+ ".");
