d570abd721abf50502626152a12117d5dc2715ea
Jacob Maier <jacobamaier@gmail.com>
2023-01-09 13:45:51 -0800

    Update User Interface Logic and Business logic.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+// business logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-// const result = add(3, 5);
-// window.alert(result);
-
-// const outputText = "The sum is " + result + ".";
-// saySomething(outputText);
-
-saySomething("The sum is " + add(3,5) + ".");
+// user interface logic
+const number1 = parseInt(prompt('Enter a number:'));
+const number2 = parseInt(prompt('Enter another number:'));
+window.alert(add(number1, number2));
