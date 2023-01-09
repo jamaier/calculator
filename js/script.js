1527b7f84130fa1f30d969f924729466c9a8909d
Jacob Maier <jacobamaier@gmail.com>
2023-01-09 14:35:23 -0800

    Reformat window alerts and add alert text announcing chosen numbers.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // business logic
 function add(number1, number2) {
   return number1 + number2;
-
 }
 
 function subtract(number1, number2) {
@@ -17,10 +16,46 @@ function mult(number1, number2) {
 }
 
 // user interface logic
-const number1 = parseInt(prompt('Enter a number:'));
-const number2 = parseInt(prompt('Enter another number:'));
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The difference of your numbers =" + subtract(number1, number2)+ ".");
-window.alert("The sum of your numbers =" + add(number1, number2)+ ".");
-window.alert("The remainder of your numbers =" + div(number1, number2)+ ".");
-window.alert("The product of your numbers =" + mult(number1, number2)+ ".");
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The difference of your numbers =" +
+    subtract(number1, number2) +
+    "."
+);
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The sum of your numbers =" +
+    add(number1, number2) +
+    "."
+);
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The remainder of your numbers =" +
+    div(number1, number2) +
+    "."
+);
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The product of your numbers =" +
+    mult(number1, number2) +
+    "."
+);
